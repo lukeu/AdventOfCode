88205652fea1e8fa731ec118224dd87aa58381a1
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-06 00:48:26 +0000

    Add EclipseTricks
    Just in a place that will be easy to spot them again in the future,
    if I need to set this up again.

---
+++ src/main/java/util/EclipseTricks.java
@@ -0,0 +1,19 @@
+package util;
+
+public class EclipseTricks {
+
+    /*
+     * Java code-completion templates...
+     *
+
+"fors" (For over characters of a String)
+
+for (int ${index} = 0; ${index} < ${s:var(java.lang.String)}.length(); ${index}++) {
+        char ch = ${s}.charAt(${index});
+        ${line_selection}${cursor}
+}
+
+
+
+     */
+}
