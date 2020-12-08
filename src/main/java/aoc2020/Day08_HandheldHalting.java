6b6898c946b66c47995716e6a2a86163a64fee69
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-08 05:34:13 +0000

    Day 8 cleaned a bit
    I wasn't sure about using switch-expressions with statements. Now I know.

--- src/main/java/aoc2020/Day08_HandheldHalting.java
+++ src/main/java/aoc2020/Day08_HandheldHalting.java
@@ -10,32 +10,35 @@ public class Day08_HandheldHalting {
     void go() {
         var in = FUtils.readLines(2020, 8);
 
-        for (int swap = 0; swap < in.size(); swap++) {
+        for (int swap = -1; swap < in.size(); swap++) {
             boolean[] exe = new boolean[in.size()];
             long acc = 0;
             int pc = 0;
             while (!exe[pc]) {
                 exe[pc] = true;
-                String[] split = in.get(pc).split(" ");
+                var split = in.get(pc).split(" ");
                 String cmd = split[0];
                 int arg = Integer.parseInt(split[1]);
+
                 if (swap != pc) {
                     switch (cmd) {
-                    case "acc": acc += arg; pc++; break;
-                    case "jmp": pc += arg; break;
-                    default: pc ++; break;
+                    case "acc" -> acc += arg;
+                    case "jmp" -> pc += (arg-1);
                     }
                 } else {
                     switch (cmd) {
-                    case "acc": acc += arg; pc++; break;
-                    case "nop": pc += arg; break;
-                    default: pc++; break;
+                    case "acc" -> acc += arg;
+                    case "nop" -> pc += (arg-1);
                     }
                 }
+                pc++;
                 if (pc == exe.length) {
                     System.out.println("Halted: acc = " + acc);
                     System.exit(0);
                 }
+            }
+            if (swap == -1) {
+                System.out.println("Part 1: acc = " + acc);
             }
         }
         System.err.println("failed");
