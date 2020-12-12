0c564c109aea0aaf7e6e4b70357c3ed30ac79cf9
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-12 22:49:21 +0100

    Day 11 - optimise 4
    Accumulate occupied seats while updating the grid, rather than
    afterwards. (19 -> 18 ms, laptop. Really scraping the barrel now...)

--- src/main/java/aoc2020/Day11_SeatingSystem.java
+++ src/main/java/aoc2020/Day11_SeatingSystem.java
@@ -1,7 +1,6 @@
 package aoc2020;
 
 import java.util.Arrays;
-import java.util.List;
 
 import framework.AocMeta;
 import framework.Base;
@@ -16,9 +15,9 @@ public class Day11_SeatingSystem extends Base {
     @Override public Object expect2() { return 2131; }
 
     int width, len;
-    List<String> in;
     char[] grid;
     char[] prev;
+    int[][] visible;
 
     @Override
     public void parse(Input input) {
@@ -37,51 +36,72 @@ public class Day11_SeatingSystem extends Base {
 
     @Override
     public Integer part2() {
+        int found = 0;
+        prev = Arrays.copyOf(grid, grid.length);
+
+        visible = new int[grid.length][];
+        for (int i = 0; i < grid.length; i++) {
+            visible[i] = findVisibleSeatIndexes(i % width, i / width);
+        }
+
         do {
             prev = Arrays.copyOf(grid, grid.length);
-            iterate();
+            found = iterate();
         } while (!Arrays.equals(prev, grid));
-
-        int found = 0;
-        for (char c : grid) {
-            if (c == '#') {
-                found++;
-            }
-        }
         return found;
     }
 
-    private void iterate() {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < len; y++) {
-                char seat = rules(x,y);
-                grid[y*width + x] = seat;
-            }
-        }
-    }
+    private static final int[] nyarp = new int[0];
 
-    private char rules(int x, int y) {
+    int[] findVisibleSeatIndexes(int x, int y) {
         char c = at(x,y);
         if (c == '.') {
-            return '.';
+            return nyarp;
         }
-        int occupied = 0;
-        for (int dx = -1; dx < 2; dx++) {
-            for (int dy = -1; dy < 2; dy++) {
+        int[] results = new int[8];
+        int i = 0;
+        for (int dy = -1; dy < 2; dy++) {
+            for (int dx = -1; dx < 2; dx++) {
                 if (dx != 0 || dy != 0) {
-                    for (int m = 1; m < width; m++) {
+                    for (int m = 1; m < width -1 ; m++) {
                         char ch = at(x+dx*m, y+dy*m);
-                        if (ch == '#') {
-                            if (c == 'L') {
-                                return 'L';
-                            }
-                            occupied ++;
-                        }
                         if (ch != '.') {
+                            if (ch != '+') {
+                                results[i++] = (y+dy*m) * width + x+dx*m;
+                            }
                             break;
                         }
                     }
                 }
+            }
+        }
+        return i == 8 ? results : Arrays.copyOf(results, i);
+    }
+
+    int iterate() {
+        int count = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < len; y++) {
+                char seat = rules(x,y);
+                grid[y*width + x] = seat;
+                if (seat == '#') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    char rules(int x, int y) {
+        char c = prev[y*width + x];
+        int occupied = 0;
+        for (int seat : visible[y*width + x]) {
+            char ch = prev[seat];
+            if (ch == '#') {
+                if (c == 'L') {
+                    return 'L';
+                }
+                occupied ++;
             }
         }
         if (c == 'L' && occupied == 0) {
