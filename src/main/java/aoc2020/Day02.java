b61524175f13ef02052f088321473864a7a3be29
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-20 16:04:11 +0000

    Optimise Day 2
    New runtime: 0.60 ms (laptop)

--- src/main/java/aoc2020/Day02.java
+++ src/main/java/aoc2020/Day02.java
@@ -1,6 +1,6 @@
 package aoc2020;
 
-import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import util.FUtils;
 import util.Util;
@@ -10,38 +10,43 @@ public class Day02 {
         Util.profile(() -> new Day02().go(), 1);
     }
 
-    // Example:
+    // Example input
     // 7-8 l: lllllzllbfl
-    // Too fragile & slow to code to - use Scanner next time.
-    // var p = Pattern.compile("(\\d+)\\-(\\d+) (.)\\: (.*)");
+
+    // Alternatives...
+    //
+    // * Too fragile & slow to code
+    //     var p = Pattern.compile("(\\d+)\\-(\\d+) (.)\\: (.*)");
+    //
+    // * Best for speed-coding: (poor perf)
+    //
+    //    var scan = new Scanner(s);
+    //    scan.useDelimiter("[-: ]+");
+    //    int min = scan.nextInt();
 
     void go() {
-        int found = 0;
+        int part1 = 0;
+        int part2 = 0;
 
+        var p = Pattern.compile("[-: ]+");
         var in = FUtils.readLines(2020, 2);
         for (String s : in) {
-            var scan = new Scanner(s);
-            scan.useDelimiter("[-: ]+");
 
-            int min = scan.nextInt();
-            int max = scan.nextInt();
-            char ch = scan.next().charAt(0);
-            String str = scan.next();
+            String[] sp = p.split(s);
+            int min = Integer.parseInt(sp[0]);
+            int max = Integer.parseInt(sp[1]);
+            char ch = sp[2].charAt(0);
+            String str = sp[3];
 
-            // TRUE = part 1
-            if (Boolean.FALSE) {
-                int count = (int) str.chars().filter(c -> c == ch).count();
-                if (count >= min && count <= max) {
-                    found ++;
-                }
-            } else {
-                if (str.charAt(min-1) == ch ^ str.charAt(max-1) == ch) {
-                    found ++;
-                }
+            int count = (int) str.chars().filter(c -> c == ch).count();
+            if (count >= min && count <= max) {
+                part1 ++;
             }
-
-            scan.close();
+            if (str.charAt(min-1) == ch ^ str.charAt(max-1) == ch) {
+                part2 ++;
+            }
         }
-        System.out.println(found);
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
     }
 }
