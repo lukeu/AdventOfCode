8929cd9f5a037c4b640b8ace02ce5d5915cf242f
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-27 20:53:13 +0000

    Day 15 - put into framework

--- src/main/java/aoc2020/Day15_RambunctiousRecitation.java
+++ src/main/java/aoc2020/Day15_RambunctiousRecitation.java
@@ -1,19 +1,36 @@
 package aoc2020;
 
-import java.util.HashMap;
-import java.util.Map;
+import framework.AocMeta;
+import framework.Base;
+import framework.Input;
 
-import util.Util;
-
-public class Day15_RambunctiousRecitation {
-    private static final int END = 30000000;
+@AocMeta(notes = "elf memory game")
+public class Day15_RambunctiousRecitation extends Base {
     public static void main(String[] args) {
-        Util.profile(new Day15_RambunctiousRecitation()::go, 1);
+        Base.run(Day15_RambunctiousRecitation::new, 1);
     }
 
-    void go() {
+    int[] in = {8,0,17,4,1,12};
+    @Override public Object expect1() { return 981L; }
+    @Override public Object expect2() { return 164878L; }
+
+    @Override
+    public Input input() {
+        return null; // No input file used
+    }
+
+    @Override
+    public Long part1() {
+        return play(2020);
+    }
+
+    @Override
+    public Long part2() {
+        return play(30000000);
+    }
+
+    long play(int numTurns) {
         Map<Integer, Integer> history = new HashMap<>();
-        var in = new int[] {8,0,17,4,1,12};
 
         int turn = 1;
         for (int i : in) {
@@ -22,13 +39,13 @@ public class Day15_RambunctiousRecitation {
         int speek = in[in.length - 1];
         history.remove(speek);
 
-        for ( ; turn <= END; turn++) {
+        for ( ; turn <= numTurns; turn++) {
             int spoken = speek;
             Integer prev = history.get(spoken);
             speek = (prev == null) ? 0 : (turn - prev - 1);
             history.put(spoken, turn - 1);
         }
 
-        System.out.println("Found: " + speek);
+        return speek;
     }
 }
