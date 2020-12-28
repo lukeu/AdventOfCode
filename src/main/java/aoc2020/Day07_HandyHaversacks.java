97cbb0dd8f6948d5dc652677da4927096816d868
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-28 20:52:41 +0000

    Day 7 - put into framework

--- src/main/java/aoc2020/Day07_HandyHaversacks.java
+++ src/main/java/aoc2020/Day07_HandyHaversacks.java
@@ -4,33 +4,45 @@ import java.util.regex.Pattern;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
-import util.FUtils;
+import framework.AocMeta;
+import framework.Base;
+import util.SUtils;
 
-public class Day07_HandyHaversacks {
+@AocMeta(notes = "recursive bags")
+public class Day07_HandyHaversacks extends Base {
     public static void main(String[] args) {
-        new Day07_HandyHaversacks().go();
+        Base.run(Day07_HandyHaversacks::new, 1);
     }
+
+    @Override public Object expect1() { return 131; }
+    @Override public Object expect2() { return 11261; }
 
     record Pair(int count, String colour) {}
 
     SetMultimap<String, Pair> rules = HashMultimap.create();
 
-    void go() {
-        for (String rule : FUtils.readLines(2020, 7)) {
-            parse(rule);
-        }
-        //System.out.println(Joiner.on("\n").join(rules.asMap().entrySet()));
-        System.out.println("Rules: " + rules.keySet().size());
-        System.out.println("Rules: " + rules.keys().size());
+    Pattern PAT = Pattern.compile("(\\d)+ (\\w+ \\w+) bag");
 
-        long found = 0;
+    @Override
+    public void parse(String text) {
+        for (String rule : SUtils.lines(text)) {
+            var parts = rule.split(" bags contain ");
+            var m = PAT.matcher(parts[1]);
+            while (m.find()) {
+                rules.put(parts[0], new Pair(Integer.parseInt(m.group(1)), m.group(2)));
+            }
+        }
+    }
+
+    @Override
+    public Object part1() {
+        int found = 0;
         for (String start : rules.keySet()) {
             if (!start.equals("shiny gold") && hasGold(start)) {
                 ++ found;
             }
         }
-        System.out.println("Has Gold: " + found);
-        System.out.println("Contents: " + countRecursive(0, "shiny gold"));
+        return found;
     }
 
     boolean hasGold(String bag) {
@@ -45,6 +57,11 @@ public class Day07_HandyHaversacks {
         return false;
     }
 
+    @Override
+    public Object part2() {
+        return countRecursive(0, "shiny gold");
+    }
+
     int countRecursive(int num, String bag) {
         var cols = rules.get(bag);
         int sum = 0;
@@ -52,15 +69,5 @@ public class Day07_HandyHaversacks {
             sum += c.count + c.count * countRecursive(c.count, c.colour);
         }
         return sum;
-    }
-
-    Pattern PAT = Pattern.compile("(\\d)+ (\\w+ \\w+) bag");
-
-    void parse(String text) {
-        var parts = text.split(" bags contain ");
-        var m = PAT.matcher(parts[1]);
-        while (m.find()) {
-            rules.put(parts[0], new Pair(Integer.parseInt(m.group(1)), m.group(2)));
-        }
     }
 }
