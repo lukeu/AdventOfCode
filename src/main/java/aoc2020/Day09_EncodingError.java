6bc538bfd1fce15f7200fe00ccf3e756e63ccc7f
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-29 20:35:10 +0000

    Day 9 - put into framework

--- src/main/java/aoc2020/Day09_EncodingError.java
+++ src/main/java/aoc2020/Day09_EncodingError.java
@@ -1,40 +1,39 @@
 package aoc2020;
 
 import java.util.Arrays;
-import java.util.List;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.common.primitives.Longs;
-import util.FUtils;
+import framework.Base;
+import framework.Input;
 import util.Util;
 
-public class Day09_EncodingError {
+public class Day09_EncodingError extends Base {
     public static void main(String[] args) {
-        Util.profile(new Day09_EncodingError()::go, 1);
+        Base.run(Day09_EncodingError::new, 1);
     }
+
+    @Override public Object expect1() { return 21806024L; }
+    @Override public Object expect2() { return 2986195L; }
 
     static final int PRE = 25;
-    long[] in;
-    List<Long> list;
+    private long[] in;
 
-    void go() {
-        in = FUtils.readLineLongs(2020, 9);
-        list = Longs.asList(in);
-
-        long invalid = part1();
-
-        part2(invalid);
+    @Override
+    public void parse(Input input) {
+        in = input.lineLongs();
     }
 
-    long part1() {
+    @Override
+    public Long part1() {
         for (int i = PRE; i < in.length; i++) {
             long val = in[i];
             if (!isSum_FAST(i)) {
                 return val;
             }
         }
-        return -1;
+        return -1L;
     }
 
     // Original - part 1 takes about 0.02 ms
@@ -51,17 +50,20 @@ public class Day09_EncodingError {
 
     // Alternative - Good to know about in case I need combinations with > 2 elements!
     // But slow... part 1 takes 3-4 ms (warm)
+    @SuppressWarnings("unused")
     private boolean isSum_ALTERNATIVE(int index) {
 
         // NB order matters - ImmutableSet and 'combinations' both retain this
         // Using a Set already filters-out identical numbers
-        ImmutableSet<Long> previous = ImmutableSet.copyOf(list.subList(index - PRE, index));
+        var previous = ImmutableSet.copyOf(Longs.asList(in).subList(index - PRE, index));
         return Sets.combinations(previous, 2)
             .stream()
             .anyMatch(set -> Util.sumBoxed(set) == in[index]);
     }
 
-    private void part2(long invalid) {
+    @Override
+    public Object part2() {
+        long invalid = part1();
         for (int s = 2; s < in.length; s++) {
             for (int i = 0; i < in.length - s; i++) {
 
@@ -70,10 +72,10 @@ public class Day09_EncodingError {
                     long min = Util.min(in, i, i+s);
                     long max = Util.max(in, i, i+s);
 
-                    System.out.println(min + max);
-                    return;
+                    return min + max;
                 }
             }
         }
+        return -1L;
     }
 }
