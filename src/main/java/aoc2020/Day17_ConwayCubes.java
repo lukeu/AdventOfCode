e189573bae2bfbd8bc69b17442cf695e6bd03744
lukeu <ldubox-coding101@yahoo.co.uk>
2020-12-30 13:50:39 +0100

    Day 17 - Shift an 'if' condition out of a hot loop
    Times 25 -> 8 ms

--- src/main/java/aoc2020/Day17_ConwayCubes.java
+++ src/main/java/aoc2020/Day17_ConwayCubes.java
@@ -85,9 +85,6 @@ public class Day17_ConwayCubes extends Base {
             for (int z = -1; z <= 1; z++) {
                 for (int y = -1; y <= 1; y++) {
                     for (int x = -1; x <= 1; x++) {
-                        if (x == 0 && y == 0 && z == 0 && w == 0) {
-                            continue;
-                        }
                         if (old[index(px + x, py + y, pz + z, pw + w)]) {
                             count++;
                         }
@@ -95,7 +92,7 @@ public class Day17_ConwayCubes extends Base {
                 }
             }
         }
-        return count;
+        return old[index(px, py, pz, pw)] ? count - 1 : count;
     }
 
     private void updateRanges(int... vv) {
