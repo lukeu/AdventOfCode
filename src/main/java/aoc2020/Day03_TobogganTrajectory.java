06659515089c75ccd8a9d7a15bbfd1c4e509fb7e
lukeu <ldubox-coding101@yahoo.co.uk>
2021-01-02 19:21:11 +0000

    Day 3 - refactor to add Part 1 back in
    This only adds like 2 us, the parsing still dominates (~38)
    with part 2 being ~8 us.

--- src/main/java/aoc2020/Day03_TobogganTrajectory.java
+++ src/main/java/aoc2020/Day03_TobogganTrajectory.java
@@ -10,40 +10,42 @@ public class Day03_TobogganTrajectory extends Base {
         Base.run(Day03_TobogganTrajectory::new, 1);
     }
 
+    @Override public Object expect1() { return 240L; }
     @Override public Object expect2() { return 2832009600L; }
 
     List<String> in;
+    int w;
 
     @Override
     public void parse(Input input) {
         in = input.lines();
+        w = in.get(0).length();
+    }
+
+    @Override
+    public Object part1() {
+        return slopeHits(1, 3);
     }
 
     @Override
     public Object part2() {
         long total = 1;
-        int w = in.get(0).length();
-        for (int i : new int []{1,3,5,7}) {
-            int x = 0;
-            int found = 0;
-            for (String s : in) {
-                if (s.charAt(x % w) == '#') {
-                    found++;
-                }
-                x += i;
-            }
-            total *= found;
+        for (int dx : new int []{1,3,5,7}) {
+            total *= slopeHits(1, dx);
         }
+        return total * slopeHits(2, 1);
+    }
 
+    private long slopeHits(int dy, int dx) {
         int x = 0;
         int found = 0;
-        for (int i = 0; i < in.size(); i += 2) {
-            String s = in.get(i);
+        for (int r = 0; r < in.size(); r += dy) {
+            String s = in.get(r);
             if (s.charAt(x % w) == '#') {
                 found++;
             }
-            x += 1;
+            x += dx;
         }
-        return total * found;
+        return found;
     }
 }
