387501e5f3722262c1d8a443f5064e311cf1cc69
lukeu <ldubox-coding101@yahoo.co.uk>
2023-12-21 22:49:08 +0000

    2015 Day 2 Part 1 - redone with C

--- src/main/java/aoc15/Old2.java
+++ src/main/java/aoc15/Old2.java
@@ -12,22 +12,21 @@ public class Old2 {
     // Version written with my daughter when she asked about programming
     void go() {
         List<String> lines = new Input(2015, 2).lines();
-        int total = 0;
+        int sum = 0;
         for (String dimension : lines) {
             System.out.println("Read: " + dimension);
             String[] dims = dimension.split("x");
             int l = Integer.parseInt(dims[0]);
             int w = Integer.parseInt(dims[1]);
             int h = Integer.parseInt(dims[2]);
-            int s1 = w*l;
-            int s2 = l*h;
-            int s3 = w*h;
-            int surface = 2*s2 + 2*s1 + 2*s3;
-            int small = Math.min (s1, s2);
-            int smallest = Math.min(small, s3);
-            int paper = surface + smallest;
-            total = total + paper;
+            var side1 = l*w;
+            var side2 = w*h;
+            var side3 = h*l;
+            int area = (side1 + side2 + side3) * 2;
+            int smallest = Math.min(Math.min(side1, side2), side3);
+            System.out.println(area + " > " + smallest);
+            sum +=area+smallest  ;
         }
-        System.out.println(total);
+        System.out.println("The grand glorious summation of all box paper, in sqaure feet before his majesty Luke Dukem is... " + sum );
     }
 }
